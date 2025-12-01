9321d866aa582b30fcd1416bccf161202acde46a
Michael Kovaliov <kovaliov@mail.tau.ac.il>
2025-12-01 17:57:56 +0200

    Add more testing

---
+++ tests/common/consts.py
@@ -0,0 +1,6 @@
+import os
+from pathlib import Path
+
+TESTS_PATH =  Path(os.path.dirname(os.path.dirname(__file__)))
+UNIT_TESTS_PATH =  TESTS_PATH / 'unit'
+UNIT_TESTS_CACHE = UNIT_TESTS_PATH / "cache"
