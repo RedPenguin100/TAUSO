e77dc35feef2c3e4758b2db29f3edf988a14a6f9
Michael Kovaliov <32510658+RedPenguin100@users.noreply.github.com>
2026-01-05 20:37:49 +0200

    mRNA half life and corollaries (#16)
    * Version bump
    
    * Re-processed data to better fit article's purpose
    
    * Added high quality mRNA half life feature
    
    * Take HBV as a mean instead of special case
    
    * Fix the "INHIBITION" column in the processed data
    
    * Setup mRNA half life in the CLI and add useful consts
    
    * Proper Mapped cell proxy
    
    * Fix data_cleanup
    
    * Added possibly relevant dependencies
    
    * Updated mRNA half life notebook
    
    * Minor correction to avoid exception

--- notebooks/consts.py
+++ notebooks/consts.py
@@ -3,6 +3,12 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(os.path.dirname(os.path.dirname(__file__)))
 NOTEBOOK_PATH = PROJECT_ROOT / 'notebooks'
+
+ORIGINAL_CSV = NOTEBOOK_PATH / 'data' / 'asoptimizer_data_original.csv'
+PROCESSED_CSV = NOTEBOOK_PATH / 'data' / 'asoptimizer_data_processed.csv'
+UPDATED_CSV = NOTEBOOK_PATH / 'data' / 'data_asoptimizer_updated.csv'
+
+
 SAVED_FEATURES = NOTEBOOK_PATH / 'saved_features'
 CACHE_DIR = PROJECT_ROOT / 'cache'
 
@@ -17,6 +23,7 @@ CELL_LINE = 'Cell_line'
 TRANSFECTION = 'Transfection'
 DENSITY = 'Density(cells/well)'
 DENSITY_UPDATED = 'Density(cells_per_well)' # Avoiding /
+PRIMER_PROBE = 'Primer_probe_set'
 MODIFICATION = 'Modification'
 PREMRNA_FOUND = 'pre_mrna_found'
 SENSE_START = 'sense_start'
