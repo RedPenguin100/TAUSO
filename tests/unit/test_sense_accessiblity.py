04fdadf000e7fb00cc85e53c3a749aa86c8b1425
Michael Kovaliov <michaelkovaliov97@gmail.com>
2026-02-02 19:29:14 +0200

    enhance testing fold

--- tests/unit/test_sense_accessiblity.py
+++ tests/unit/test_sense_accessiblity.py
@@ -3,14 +3,14 @@ import numpy as np
 
 import pandas as pd
 
-from tauso.features.rna_access.rna_access import RNAAccess, parse
 from tauso.new_model.consts_dataframe import *
-from tauso.features.feature_names import SENSE_START, SENSE_LENGTH, SENSE_START_FROM_END
-from tauso.new_model.populate.populate_sense_accessibility import populate_sense_accessibility, SENSE_AVG_ACCESSIBILITY
+from tauso.features.names import SENSE_START, SENSE_LENGTH, SENSE_START_FROM_END
+from tauso.populate.populate_fold import populate_sense_accessibility, SENSE_AVG_ACCESSIBILITY, \
+    populate_sense_accessibility_batch
 
 from tauso.new_model.consts_dataframe import SEQUENCE
 from tauso.util import get_antisense
-from tests.common.consts import UNIT_TESTS_PATH
+from tests.unit.conftest import SHORT_GENE
 
 FLANK_SIZE = 120
 ACCESS_SIZE = 13
@@ -38,12 +38,35 @@ def get_init_df(target_mrna, aso_sizes=[16, 20], canonical_name='DDX11L1'):
 
 
 @pytest.mark.slow
-def test_regression(short_mrna, n_compare=10, path="avg_sense_access.txt", use_saved=True):
-    df = get_init_df(short_mrna.full_mrna, [16])
-    print("Length mRNA: ", len(short_mrna.full_mrna))
+def test_regression(small_gene_to_data, n_compare=10, path="avg_sense_access.txt", use_saved=True):
+    df = get_init_df(small_gene_to_data[SHORT_GENE].full_mrna, [16])
+    print("Length mRNA: ", len(small_gene_to_data[SHORT_GENE].full_mrna))
 
-    populate_sense_accessibility(df, short_mrna)
+    populate_sense_accessibility(df, small_gene_to_data)
     avg_sense_predictions = list(df[SENSE_AVG_ACCESSIBILITY])
+
+    if not use_saved:
+        # write predictions so they can serve as regression data
+        with open(path, "w") as f:
+            for val in avg_sense_predictions:
+                f.write(f"{val if val is not None else 'nan'}\n")
+
+            avg_sense_regression = np.array(avg_sense_predictions)
+    else:
+        avg_sense_regression = np.loadtxt(path)
+    # compare
+    np.testing.assert_allclose(
+        np.array(avg_sense_predictions[:n_compare]),
+        np.array(avg_sense_regression[:n_compare])
+    )
+
+@pytest.mark.slow
+def test_regression_batch(small_gene_to_data, n_compare=10, path="avg_sense_access.txt", use_saved=True):
+    df = get_init_df(small_gene_to_data[SHORT_GENE].full_mrna, [16])
+    print("Length mRNA: ", len(small_gene_to_data[SHORT_GENE].full_mrna))
+
+    df, feature_name = populate_sense_accessibility_batch(df, small_gene_to_data)
+    avg_sense_predictions = list(df[feature_name])
 
     if not use_saved:
         # write predictions so they can serve as regression data
