db887112905ab75f10e972c4573b3d745bb001f2
Michael Kovaliov <michaelkovaliov97@gmail.com>
2026-02-02 18:58:16 +0200

    Move function to fold

--- src/tauso/populate/populate_fold.py
+++ src/tauso/populate/populate_fold.py
@@ -1,5 +1,11 @@
+import uuid
 import numpy as np
+import pandas as pd
 from pandarallel import pandarallel
+
+from ..features.rna_access.access_calculator import get_cache, get_sense_with_flanks
+from ..features.rna_access.sense_accessibility import compute_sense_accessibility_value
+from ..features.rna_access.access_calculator import AccessCalculator
 
 from ..data.consts import CANONICAL_GENE
 from ..features.fold.vienna_fold import calculate_avg_mfe_over_sense_region
@@ -68,3 +74,148 @@ def populate_mfe_features(df, gene_to_data, n_jobs=1):  # 2. Add n_jobs param
             df[feature_name] = df.apply(_process_row, axis=1)
 
     return df, feature_names
+
+
+SENSE_AVG_ACCESSIBILITY = 'sense_avg_accessibility'
+
+# --- CONSTANTS ---
+FLANK_SIZE = 120
+ACCESS_SIZE = 13
+SEED_SIZE = 13
+SEED_SIZES = [SEED_SIZE * m for m in range(1, 4)]
+ACCESS_WIN_SIZE = 80
+
+
+def populate_sense_accessibility(aso_dataframe, gene_to_data):
+    access_cache = get_cache(SEED_SIZES, access_size=ACCESS_SIZE)
+
+    valid_rows = [
+        (idx, row) for idx, row in aso_dataframe.iterrows()
+        if row[SENSE_START] != -1
+    ]
+
+    for idx, row in valid_rows:
+        sense_start = row[SENSE_START]
+        sense_length = row[SENSE_LENGTH]
+        full_mrna_seq = gene_to_data(row[CANONICAL_GENE].full_mrna)
+
+        flanked_sense = get_sense_with_flanks(
+            full_mrna_seq, sense_start, sense_length,
+            flank_size=FLANK_SIZE
+        )
+
+        if not flanked_sense or len(flanked_sense) < ACCESS_SIZE:
+            aso_dataframe.loc[idx, SENSE_AVG_ACCESSIBILITY] = 0
+            continue
+
+        avg_sense_access = compute_sense_accessibility_value(
+            sense_start, sense_length, flank=flanked_sense, flank_size=FLANK_SIZE, access_win_size=ACCESS_WIN_SIZE,
+            seed_sizes=SEED_SIZES, access_size=ACCESS_SIZE, cache=access_cache
+        )
+        aso_dataframe.loc[idx, SENSE_AVG_ACCESSIBILITY] = avg_sense_access
+
+
+def populate_sense_accessibility_batch(aso_dataframe, gene_to_data, batch_size=1000, flank_size=FLANK_SIZE,
+                                       access_size=ACCESS_SIZE,
+                                       seed_sizes=SEED_SIZES,
+                                       access_win_size=ACCESS_WIN_SIZE,
+                                       n_jobs=1):
+    # 1. New Naming Scheme: access_120flank_13access_4-6-8seed_sizes
+    seeds_list = seed_sizes if isinstance(seed_sizes, (list, tuple)) else [seed_sizes]
+    seeds_str = "-".join(map(str, seeds_list))
+    feature_name = f"access_{flank_size}flank_{access_size}access_{seeds_str}seed_sizes"
+
+    # Setup Output
+    df_out = aso_dataframe.copy()
+    df_out[feature_name] = np.nan
+
+    valid_mask = df_out[SENSE_START] != -1
+    if not valid_mask.any():
+        return df_out, feature_name
+
+    # Prepare minimal data for workers (keeping index)
+    valid_df = df_out.loc[valid_mask, [SENSE_START, SENSE_LENGTH, CANONICAL_GENE]].copy()
+    access_cache = get_cache(seed_sizes, access_size=access_size)
+
+    # 2. Worker Function
+    def _process_batch(batch_rows):
+        rna_seqs = []
+        sense_len_map = {}
+        sense_start_map = {}
+        batch_uuid = str(uuid.uuid4())
+
+        for idx, row in batch_rows.iterrows():
+            current_id = str(idx)  # Use original index as ID
+
+            full_mrna_seq = str(gene_to_data[row[CANONICAL_GENE]].full_mrna)
+            current_sense_start = row[SENSE_START]
+            flank_start = max(0, current_sense_start - flank_size)
+            relative_start = current_sense_start - flank_start
+
+            flanked_sense = get_sense_with_flanks(
+                full_mrna_seq,
+                current_sense_start,
+                row[SENSE_LENGTH],
+                flank_size=flank_size
+            )
+
+            if not flanked_sense or len(flanked_sense) < access_size:
+                continue
+
+            flanked_sense = flanked_sense.upper().replace('T', 'U')
+            rna_seqs.append((current_id, flanked_sense))
+            sense_len_map[current_id] = row[SENSE_LENGTH]
+            sense_start_map[current_id] = relative_start
+
+        if not rna_seqs:
+            return pd.Series(dtype=float, name=feature_name)
+
+        df = AccessCalculator.calc_new(
+            rna_seqs,
+            access_size=access_size,
+            min_gc=0,
+            max_gc=100,
+            gc_ranges=1,
+            uuid_str=batch_uuid,
+            access_win_size=access_win_size,
+            access_seed_sizes=seed_sizes,
+            cache=access_cache
+        )
+
+        df['pos_in_flank'] = df.groupby('rna_id').cumcount()
+        df['sense_length'] = df['rna_id'].map(sense_len_map)
+        df['rel_start'] = df['rna_id'].map(sense_start_map)
+
+        mask = (
+                (df['pos_in_flank'] >= df['rel_start']) &
+                (df['pos_in_flank'] < df['rel_start'] + df['sense_length'])
+        )
+
+        # Aggregate and restore index type
+        batch_result = (
+            df.loc[mask]
+            .groupby('rna_id')['avg_access']
+            .mean()
+        )
+        batch_result.index = batch_result.index.astype(batch_rows.index.dtype)
+        batch_result.name = feature_name
+
+        return batch_result
+
+    # 3. Execution
+    valid_df['batch_group'] = np.arange(len(valid_df)) // batch_size
+
+    if n_jobs > 1:
+        from pandarallel import pandarallel
+        pandarallel.initialize(nb_workers=n_jobs, verbose=0)
+        results_series = valid_df.groupby('batch_group').parallel_apply(_process_batch)
+    else:
+        results_series = valid_df.groupby('batch_group').apply(_process_batch)
+
+    if isinstance(results_series.index, pd.MultiIndex):
+        results_series = results_series.droplevel(0)
+
+    # 4. Safe Assignment (Automatic Alignment)
+    df_out.loc[results_series.index, feature_name] = results_series
+
+    return df_out, feature_name
