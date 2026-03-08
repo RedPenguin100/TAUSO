ed0e01d3da1a70a5e0d7484dde3ad7347e70216b
Michael Kovaliov <michaelkovaliov97@gmail.com>
2026-03-08 20:34:17 +0200

    Fix missing data

---
+++ notebooks/utils/data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+INDEXED_OLIGO_DATA = 'aso_inhibitions_with_canonical_gene_indexed.csv'
+OLIGO_DATA = 'aso_inhibitions_with_canonical_gene.csv.gz'
+
+def read_oligo_indexed_data():
+    data = pd.read_csv(OLIGO_DATA)
+
+    # Check if the column exists
+    if 'index_oligo' not in data.columns:
+        # Create the column starting at 1
+        data['index_oligo'] = range(1, len(data) + 1)
+
+        # Save the file
+        data.to_csv('aso_inhibitions_with_canonical_gene_indexed.csv', index=False)
+        print("Added 'index_oligo' and saved file.")
+    else:
+        print("'index_oligo' already exists. No changes made.")
+
+    return data
+
+if __name__ == "__main__":
+    read_oligo_indexed_data()
