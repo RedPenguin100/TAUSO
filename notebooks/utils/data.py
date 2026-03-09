80108e7bc4ecd190c61e83cd70ee2aed53d74a75
Michael Kovaliov <michaelkovaliov97@gmail.com>
2026-03-09 10:24:49 +0200

    Fix

--- notebooks/utils/data.py
+++ notebooks/utils/data.py
@@ -1,24 +1,24 @@
 import pandas as pd
 
-from notebooks.consts import PROCESSED_OLIGO_CSV_GZ
+from notebooks.consts import PROCESSED_OLIGO_CSV_GZ, OLIGO_CSV_INDEXED
 
-INDEXED_OLIGO_DATA = 'aso_inhibitions_with_canonical_gene_indexed.csv'
 
 def read_oligo_indexed_data():
     data = pd.read_csv(PROCESSED_OLIGO_CSV_GZ)
 
     # Check if the column exists
-    if 'index_oligo' not in data.columns:
+    if "index_oligo" not in data.columns:
         # Create the column starting at 1
-        data['index_oligo'] = range(1, len(data) + 1)
+        data["index_oligo"] = range(1, len(data) + 1)
 
         # Save the file
-        data.to_csv('aso_inhibitions_with_canonical_gene_indexed.csv', index=False)
+        data.to_csv(OLIGO_CSV_INDEXED, index=False)
         print("Added 'index_oligo' and saved file.")
     else:
         print("'index_oligo' already exists. No changes made.")
 
     return data
+
 
 if __name__ == "__main__":
     read_oligo_indexed_data()
