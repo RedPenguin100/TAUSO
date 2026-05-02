a20f268808a8f25fe3e248eb8f78e9d00bd0dff7
Michael Kovaliov <michaelkovaliov97@gmail.com>
2026-05-02 12:38:27 +0300

    Add logging to the generate_aso_features

--- src/tauso/aso_generation.py
+++ src/tauso/aso_generation.py
@@ -124,10 +124,16 @@ def generate_stub_data(
     return data
 
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 def generate_aso_features(data, cache: AssetCache, n_jobs=1, get_feature_dir_func=None):
     original_columns = set(data.columns)
 
-    print("version is None, not saving features to disk")
+    logger.info("version is None, not saving features to disk")
+
     calculator = Calculator(
         data=data, data_version=None, overwrite=True, cpus=n_jobs, cache=cache, get_feature_dir=get_feature_dir_func
     )
