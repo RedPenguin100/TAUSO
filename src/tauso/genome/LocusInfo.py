5038485230d473fad44c80dc5fb9c81213819f98
Michael Kovaliov <michaelkovaliov97@gmail.com>
2026-05-04 19:20:45 +0300

    Memory optimization

--- src/tauso/genome/LocusInfo.py
+++ src/tauso/genome/LocusInfo.py
@@ -1,13 +1,12 @@
 class LocusInfo:
     def __init__(self, seq=None):
-        self.exons = []
-        self.introns = []
+        # Memory Optimization: We no longer store lists of strings for exons/introns.
+        # These are computed on the fly via @property decorators.
         self.exon_indices = []
         self.intron_indices = []
         self.stop_codons = []
         self.five_prime_utr = ""
         self.three_prime_utr = ""
-        self.exon_concat = None
         self.full_mrna = None
         self.gene_start = None
         self.gene_end = None
@@ -16,16 +15,45 @@ class LocusInfo:
         self.utr_indices = []
 
         if seq is not None:
-            self.exons = [seq]
-            self.exon_indices = [(0, len(seq) - 1)]
+            self.exon_indices = [(0, len(seq))]
             self.gene_start = 0
-            self.gene_end = len(seq) - 1
+            self.gene_end = len(seq)
             self.strand = "+"
             self.gene_type = "unknown"
-            self.exon_concat = seq
             self.full_mrna = seq
+
+    def _get_sequence_slice(self, start, end):
+        """Helper to extract a sequence from the full_mrna based on genomic coordinates."""
+        if not self.full_mrna or self.gene_start is None or self.gene_end is None:
+            return ""
+
+        if self.strand == "+":
+            # Relative to the 5' end of the + strand (gene_start)
+            rel_start = start - self.gene_start
+            rel_end = end - self.gene_start
+        else:
+            # Relative to the 5' end of the - strand (gene_end)
+            # Because full_mrna is reverse complemented, we map from the right side.
+            rel_start = self.gene_end - end
+            rel_end = self.gene_end - start
+
+        return self.full_mrna[rel_start:rel_end]
+
+    @property
+    def exons(self):
+        return [self._get_sequence_slice(start, end) for start, end in self.exon_indices]
+
+    @property
+    def introns(self):
+        return [self._get_sequence_slice(start, end) for start, end in self.intron_indices]
+
+    @property
+    def exon_concat(self):
+        return "".join(self.exons)
 
     def __repr__(self):
         print("LocusInfo:")
+        # We use a custom print to avoid aggressively computing properties just for a representation
         for field, value in self.__dict__.items():
             print(f"  {field}: {value}")
+        return ""
--- src/tauso/genome/read_human_genome.py
+++ src/tauso/genome/read_human_genome.py
@@ -28,7 +28,6 @@ def get_locus_to_data_dict(include_introns=True, gene_subset=None, genome="GRCh3
     if include_introns:
         feature_types.append("intron")
 
-    # --- OPTIMIZATION START ---
     iterator = []
 
     # If we have specific genes, don't scan the whole genome.
@@ -49,22 +48,20 @@ def get_locus_to_data_dict(include_introns=True, gene_subset=None, genome="GRCh3
                 children = db.children(gene, featuretype=feature_types, order_by="start")
                 iterator.extend(list(children))
     else:
-        # Full genome scan
         iterator = db.features_of_type(feature_types, order_by="start")
-    # --- OPTIMIZATION END ---
 
     for feature in iterator:
         chrom = feature.seqid
         if "chrM" == chrom:
             continue
+
         locus_tags = feature.attributes["gene_name"]
         if len(locus_tags) != 1:
             raise ValueError(f"Multiple loci: {locus_tags}")
         locus_tag = locus_tags[0]
 
-        if gene_subset is not None:
-            if locus_tag not in gene_subset:
-                continue
+        if gene_subset is not None and locus_tag not in gene_subset:
+            continue
 
         if locus_tag not in locus_to_data:
             locus_info = LocusInfo()
@@ -73,67 +70,44 @@ def get_locus_to_data_dict(include_introns=True, gene_subset=None, genome="GRCh3
             locus_info = locus_to_data[locus_tag]
 
         if feature.featuretype == "exon":
-            exon = feature
-            # seq = fasta_dict[chrom].seq[exon.start - 1: exon.end]
-            seq = Seq(str(fasta_dict[chrom][exon.start - 1 : exon.end]))
-            if exon.strand == "-":
-                seq = seq.reverse_complement()
-            seq = str(seq).upper()
-
-            bisect.insort(locus_info.exons, (exon.start - 1, seq))
-            bisect.insort(locus_info.exon_indices, (exon.start - 1, exon.end))
-            locus_to_strand[locus_tag] = exon.strand
+            # Just save the coordinates. The actual sequence is derived lazily.
+            bisect.insort(locus_info.exon_indices, (feature.start - 1, feature.end))
+            locus_to_strand[locus_tag] = feature.strand
 
         elif feature.featuretype == "intron" and include_introns:
-            intron = feature
-            # seq = fasta_dict[chrom].seq[intron.start - 1: intron.end]
-            seq = Seq(str(fasta_dict[chrom][intron.start - 1 : intron.end]))
-
-            if intron.strand == "-":
-                seq = seq.reverse_complement()
-            seq = str(seq).upper()
-
-            bisect.insort(locus_info.introns, (intron.start - 1, seq))
-            bisect.insort(locus_info.intron_indices, (intron.start - 1, intron.end))
-            locus_to_strand[locus_tag] = intron.strand
+            bisect.insort(locus_info.intron_indices, (feature.start - 1, feature.end))
+            locus_to_strand[locus_tag] = feature.strand
 
         elif feature.featuretype == "gene":
-            gene = feature
-            # seq = fasta_dict[chrom].seq[gene.start - 1: gene.end]
-            seq = Seq(str(fasta_dict[chrom][gene.start - 1 : gene.end]))
-
-            if gene.strand == "-":
+            # We ONLY pull the sequence into memory at the Gene level
+            seq = Seq(str(fasta_dict[chrom][feature.start - 1 : feature.end]))
+            if feature.strand == "-":
                 seq = seq.reverse_complement()
-            seq = str(seq).upper()
 
-            locus_info.strand = gene.strand
-            locus_info.gene_start = gene.start - 1
-            locus_info.gene_end = gene.end
-            locus_info.full_mrna = seq
-            locus_to_strand[locus_tag] = gene.strand
+            locus_info.strand = feature.strand
+            locus_info.gene_start = feature.start - 1
+            locus_info.gene_end = feature.end
+            locus_info.full_mrna = str(seq).upper()
+            locus_to_strand[locus_tag] = feature.strand
 
-            raw_type = gene.attributes.get("gene_type", gene.attributes.get("gene_biotype", ["unannotated"]))
-
-            # gffutils stores attributes as lists (e.g., ["protein_coding"]), so we grab the first element.
+            raw_type = feature.attributes.get("gene_type", feature.attributes.get("gene_biotype", ["unannotated"]))
             locus_info.gene_type = raw_type[0] if raw_type else "unannotated"
 
         elif "UTR" in feature.featuretype:
-            utr = feature
-            bisect.insort(locus_info.utr_indices, (utr.start - 1, utr.end))
+            bisect.insort(locus_info.utr_indices, (feature.start - 1, feature.end))
+
         elif feature.featuretype == "stop_codon":
             locus_info.stop_codons.append((feature.start, feature.end))
+
         else:
-            logger.debug("[Get_Locus] Feature type: ", feature.featuretype)
+            logger.debug(f"[Get_Locus] Feature type: {feature.featuretype}")
 
-    for locus_tag in locus_to_data:
-        locus_info = locus_to_data[locus_tag]
-        if locus_to_strand[locus_tag] == "-":
-            locus_info.exons.reverse()
+    # Final cleanup: Reverse coordinates for the negative strand so
+    # exon_indices go biologically 5' to 3'.
+    for locus_tag, locus_info in locus_to_data.items():
+        if locus_to_strand.get(locus_tag) == "-":
+            locus_info.exon_indices.reverse()
             if include_introns:
-                locus_info.introns.reverse()
-        locus_info.exons = [element for _, element in locus_info.exons]
-
-        if include_introns:
-            locus_info.introns = [element for _, element in locus_info.introns]
+                locus_info.intron_indices.reverse()
 
     return locus_to_data
